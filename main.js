3f9dcfba09472435a4eaf2e54ab5d6ceeb4750a3
Steven Funk <steven.funk@live.com>
2023-04-04 14:32:47 -0600

    Initial commit

---
+++ main.js
@@ -0,0 +1,23 @@
+
+
+var path = require('path');
+var express = require('express')
+var serveStatic = require('serve-static')
+var app = express()
+
+const http = require('http');
+const server = http.createServer(app);
+
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+
+var htmlPath = path.join(__dirname, 'html');
+console.log(htmlPath)
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+});
+
+app.use(serveStatic(htmlPath));
+server.listen(80);
