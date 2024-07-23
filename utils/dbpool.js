ce41338b5049f91090472d07c0942f29b1c37451
Steven Funk <steven.funk@live.com>
2024-07-23 15:05:38 -0600

    Update dbpool.js

--- utils/dbpool.js
+++ utils/dbpool.js
@@ -6,13 +6,9 @@ const mysql = require('mysql');
 const myauthobj = require('../ignore/auth.js').dbmain;
 
 myauthobj.connectionLimit = 10;
-const imagepoolconfig = {...myauthobj};
-imagepoolconfig.database = 'smileinducingstickerimages';
-const imagepool = mysql.createPool(imagepoolconfig);
 const pool = mysql.createPool(myauthobj);
 
 
 module.exports = {
-	pool,
-	imagepool
+	pool
 };
